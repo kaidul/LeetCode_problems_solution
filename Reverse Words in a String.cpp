52218222809d6d7978efb2b799ed1a9bd71f891a
Kaidul Islam <ikaidul@yahoo.com>
2014-09-21 17:54:28 +0600

    add first solution

---
+++ Reverse Words in a String.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void reverseWords(string &s) {
+        stack <string> words;
+        int start;
+        string tmp;
+        for(int i = 0, n = s.length(); i < n; ++i) {
+            start = i;
+            while(s[i] and s[i] != ' ') ++i;
+            tmp = s.substr(start, i - start);
+            if(!tmp.empty()) words.push(tmp);
+        }
+        s = "";
+        string pop;
+        while(!words.empty()) {
+            pop = words.top(), words.pop();
+            s += pop + " ";
+        }
+        if(!s.empty()) s.erase(s.length() - 1, 1);
+    }
+};
