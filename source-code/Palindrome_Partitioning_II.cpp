91beb4aeca4c8583f05cb11ade0cebe96ae96dd1
kaidul <kaidulislam90@gmail.com>
2017-10-17 10:25:22 +0600

    Palindrome partitioning II top down solution implemented

--- source-code/Palindrome_Partitioning_II.cpp
+++ source-code/Palindrome_Partitioning_II.cpp
@@ -1,6 +1,25 @@
 class Solution {
+    int minCut(int i, int j, string const& s, vector<vector<int>>& dp) {
+        if(i >= j) {
+            return 0;
+        }
+        if(dp[i][j] != INT_MAX) {
+            return dp[i][j];
+        }
+        if(s[i] == s[j] and minCut(i + 1, j - 1, s, dp) == 0) {
+            return dp[i][j] = 0;
+        }
+        int Min = j - i;
+        for(int k = i; k < j; k++) {
+            Min = min(Min, 1 + minCut(i, k, s, dp) + minCut(k + 1, j, s, dp));
+        }
+        return dp[i][j] = Min;
+    }
 public:
     int minCut(string s) {
+        // vector<vector<int>> dp(s.length(), vector<int>(s.length(), INT_MAX));
+        // return minCut(0, s.length() - 1, s, dp);
+        
         const int SIZE = s.length();
         vector<int> minCuts(SIZE + 1, 0);
         for (int i = 0; i <= SIZE; i++) {
